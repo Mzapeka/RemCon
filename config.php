a2313e3900b687df11c8a19ea26343b853ae493c
Mzapeka <mzapeka@gmail.com>
2016-07-29 17:55:00 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,6 @@
 <?php
 
-/*define("SITE", "http://kts540.in.ua/");
-define("MYMAIL", "nikolay.zapeka@ua.bosch.com");
-define("ADMINMAIL", "promo@kts540.in.ua");*/
-
 define('MYSITE', 'http://remcon/');
-//define("SITE", "http://kts540.in.ua/");
-//define("MYMAIL", "mzapeka@gmail.com");
-//define("ADMINMAIL", "promo@kts540.in.ua");
 
 //User roles
 define("USER", "user");
@@ -20,19 +13,3 @@ define("MQTT_IDCLIENT", "Remout Condition Service");
 define("MQTT_PASS", "test");
 define("MQTT_LOGIN", "test");
 
-//База данных
-
-//define ("DBHOST", "");
-//define ("DBLOGIN", "");
-//define ("DBPASS", "");
-//define ("DBNAME", "");
-//define ("DBTYPE", "");
-
-//Настройки SMTP
-
-//define ("SMTPHOST", "ssl://smtp.gmail.com");
-//define ("SMTPLOGIN", "bosch.adw@gmail.com");
-//define ("SMTPPASS", "Bosch2015");
-//define ("SMTPORT", "465");
-
-//Админка
